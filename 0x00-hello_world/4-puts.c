927754e9f164b191b080c3b4d80059316fcd64ec
ruth ambogo <ruthambogo.ra@gmail.com>
2023-02-16 07:24:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-this is the entry point
+ * Return:o(success)
+ */
+int main (void)
+{
+	puts("/"Programming is like building a multilingual puzzle);
+	return (0);
+}
