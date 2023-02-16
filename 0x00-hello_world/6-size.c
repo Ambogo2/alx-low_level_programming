ea92a4be335f53a63612221c1091c76d68f4cce2
ruth ambogo <ruthambogo.ra@gmail.com>
2023-02-16 09:59:07 -0800

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** main- Entry
+ * Return: Always zero
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n",sizeof(long int));
+	printf("size of
