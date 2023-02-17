587aa6547c1a52c07ce19cf7e002b411cb33e71f
ruth ambogo <ruthambogo.ra@gmail.com>
2023-02-17 06:57:43 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	char i:
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
