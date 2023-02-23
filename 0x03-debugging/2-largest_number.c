2336dc74a0ce8dad2d0f8f185e9ae8fc17146680
ruth ambogo <ruthambogo.ra@gmail.com>
2023-02-23 11:25:41 -0800

    3-main_a.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,12 +16,19 @@ if (a > b && b > c)
 {
 	largest = a;
 }
+else if (a > c && c > b)
+	largest = a;
 else if (b > a && a > c)
 {
 	largest = b;
 }
-else
+else if (b > c && c > a)
+	largest = b;
+}
+else if (c > a && a > b)
 {
+	largest = c;
+else if (c > b && b > a)
 	largest = c;
 }
 
