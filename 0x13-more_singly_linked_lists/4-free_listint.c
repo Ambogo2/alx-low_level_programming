9503055cd6bad3f8b3b2a258a88de63947cf3ae0
ruth ambogo <ruthambogo.ra@gmail.com>
2023-04-03 06:14:12 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
