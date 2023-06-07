7368fae1f213a6224a3e89f7733e80ecd1d175ca
Ambogo2 <ruthambogo.ra@gmail.com>
2023-06-07 10:07:40 -0700

    compare

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * wildcmp- compares two strings
+ * @s1:argument 1
+ * @s2:argument 2
+ * Return:0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s2 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
+
