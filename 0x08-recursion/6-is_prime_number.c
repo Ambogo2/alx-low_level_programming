358251086caf68ed0c88d20411a6412a28d44e33
Ambogo2 <ruthambogo.ra@gmail.com>
2023-06-07 07:26:11 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 /**
  * is_prime_number- finds the prime number
  * @n:Argument
- * Return- return 1 if it is an integer otherwise return 0
+ * Return:return 1 if it is a prime number otherwise return 0
  */
 
 int is_prime_number(int n)
