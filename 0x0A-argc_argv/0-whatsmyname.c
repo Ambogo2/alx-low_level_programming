f2c0331c79ee8446da8519ac1618c08b879d76e3
Ambogo2 <ruthambogo.ra@gmail.com>
2023-06-09 02:25:57 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a function that takes two argumets
+ * @argc:argument 1
+ * @argv:argument 2
+ * Return:Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
