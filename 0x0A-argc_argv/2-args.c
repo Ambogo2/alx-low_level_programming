ede1a24d827b0e60fa8421a7129963a82acacd24
Ambogo2 <ruthambogo.ra@gmail.com>
2023-06-09 06:59:11 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,10 +11,13 @@
 int main(int argc, char *argv[])
 {
 	int i;
-
-	for (i = 0; i < argc; i++)
+	
+	if (argc > 0)
 	{
-		printf("%s \n", argv[i]);
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s \n", argv[i]);
+		}
 	}
 	return (0);
 }
