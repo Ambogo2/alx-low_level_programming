27a8da4c3526653e0386d3e83c63fa4567d21bf0
Ambogo2 <ruthambogo.ra@gmail.com>
2023-06-21 11:46:11 -0700

    tasks

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+*main - main function
+*@argc:number of arguments
+*@argv:array of poniters to the argument
+*Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op_fun)(int, int);
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	op_fun = get_op_func(operator);
+
+	if (op_fun == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = op_fun(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+
+/**
+*op_add - returns the sum of a and b
+*@a:first argument
+*@b:second argument
+*Return:sum of a and b
+*/
+
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*op_sub - returns the differece of a and b
+*@a:first argument
+*@b:second argument
+*Return:differece of a and b
+*/
+
+
+int op_sub(int a, int b)
+
+{
+	return (a - b);
+}
+/**
+*op_mul - returns the multiplication of a and b
+*@a:first argument
+*@b:second argument
+*Return:multiplication of a and b
+*/
+
+int op_mul(int a, int b)
+
+{
+	return (a * b);
+}
+/**
+*op_div -returns the division of a and b
+*@a:first argument
+*@b:second argument
+*Return:division of a and b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+*op_mod - returns the modulo of a and b
+*@a:first argument
+*@b:second argument
+*Return:modulo of a and b
+*/
+
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
