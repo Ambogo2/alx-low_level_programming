4ddc0298ce498e82366b3efd645244c46a2e31d4
Ambogo2 <ruthambogo.ra@gmail.com>
2023-06-29 00:21:16 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,8 +8,19 @@
 
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
+	{
 		return (0);
-	else
-		return (is_prime_number n (n - 1));
+	}
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+
+	return (1);
 }
