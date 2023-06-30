716bff0ef3c67072e90c28eb4863d46c2fd58e7d
Ambogo2 <ruthambogo.ra@gmail.com>
2023-06-30 06:35:48 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - Function executed before main is exucuted
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
