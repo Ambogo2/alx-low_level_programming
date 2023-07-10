080fa35c8e17d0bdae1e0b0a975f77334ad42394
Ambogo2 <ruthambogo.ra@gmail.com>
2023-07-10 03:06:47 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - creates a file
+ * @filename: a pointer to the file to be created
+ * @text_content: a pointer to the string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+
+	int fd, w, len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			continue;
+	}
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
