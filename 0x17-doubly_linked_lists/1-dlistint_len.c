dac8eb212ace896cd6bac0cd602c02b5c0aabcba
Ambogo2 <ruthambogo.ra@gmail.com>
2023-08-17 07:18:27 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+*dlistint_len - a function that finds the length of elements
+*@h:pointer to the list to find the length
+*Return:The number of elements in a linked list
+*/
+
+ssize_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return(count);
+}
