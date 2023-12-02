ad0dd153669b03e7778abe133916e11cd9aeaf87
Ambogo2 <ruthambogo.ra@gmail.com>
2023-12-02 02:29:46 -0800

    hashtable

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,20 +9,20 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int idx;
+	unsigned long int idx;
 
-    if (ht == NULL || key == NULL)
-    {
-        return (0);
-    }
-    idx = key_index((const unsigned char *)key, ht->size);
-    while(ht->array[idx] != NULL)
-    {
-        if (*(ht->array[idx]->key) == *key)
-        {
-            return (ht->array[idx]->value);
-        }
-        ht->array[idx] = ht->array[idx]->next;
-    }
-    return (NULL);
+	if (ht == NULL || key == NULL)
+	{
+		return (0);
+	}
+	idx = key_index((const unsigned char *)key, ht->size);
+	while(ht->array[idx] != NULL)
+	{
+		if (*(ht->array[idx]->key) == *key)
+		{
+			return (ht->array[idx]->value);
+		}
+		ht->array[idx] = ht->array[idx]->next;
+	}
+	return (NULL);
 }
