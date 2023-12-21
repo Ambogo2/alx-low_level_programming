0152773813d45177edde6c5c07af407e03e77023
Ambogo2 <ruthambogo.ra@gmail.com>
2023-12-21 11:51:49 +0300

    commit

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+6-hash_table_delete.c
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - a function that deletes a hash table.
+ * @ht:the hashtable
+ * Return:void
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp, *next;
+	if (ht == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+ 		while (temp != NULL)
+    		{
+			next = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+    
+}
