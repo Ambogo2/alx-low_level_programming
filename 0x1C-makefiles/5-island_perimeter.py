ddb137ff0ba78be9f7f85bb27d220b73b936d410
Ambogo2 <ruthambogo.ra@gmail.com>
2024-03-20 12:19:42 +0300

    updated pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines island_primeter function"""
 
+
 def island_perimeter(grid):
     """
     island_perimter:Returns the perimeter of an island
@@ -10,16 +11,16 @@ def island_perimeter(grid):
         The perimeter  of the  island
     """
 
-    width =len(grid[0])
+    width = len(grid[0])
     height = len(grid)
-    edges  = 0
+    edges = 0
     size = 0
 
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
-                size  += 1
-                if (j  > 0 and grid[i][j - 1] == 1):
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
